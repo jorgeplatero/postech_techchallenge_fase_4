efe0ca531584b05a208584950141efd6f3585b0e
jorgeplatero <jorge.platero@outlook.com>
2024-01-13 22:36:28 -0300

    atualizando MVP

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.markdown(
     '''
         <div style="text-align: justify;">
             <p>
-                Este projeto tem por objetivo o desenvolvimento de um dashboard interativo capaz de gerar insights relevantes para tomada de decisão no que diz respeito ao negócio do petróleo brent, o que inclui a implementação de um modelo de Machine Learning que traga o forecasting do preço do petróleo.
+                Este projeto tem por objetivo o desenvolvimento de um dashboard interativo capaz de gerar insights relevantes para tomada de decisão no que diz respeito ao negócio do petróleo brent, o que inclui a implementação de um modelo de Machine Learning que traga o forecasting dos preços.
             </p>
             <p>
                 Esta aplicação é um MVP. O projeto completo está disponível em <b><a style='text-decoration:none', href='https://github.com/jorgeplatero/postech_techchallenge_fase_4'>repositório</a></b> GitHub.
