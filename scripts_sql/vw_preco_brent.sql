efb03a4a9b7961828ac5d2a972b9464b05a88e05
Mateus Albuquerque <105314725+mateus-albuquerque@users.noreply.github.com>
2024-01-17 23:58:42 -0300

    Atualização da View

--- scripts_sql/vw_preco_brent.sql
+++ scripts_sql/vw_preco_brent.sql
@@ -1,26 +1,29 @@
--- View: public.vw_preco_brent
-
--- DROP VIEW public.vw_preco_brent;
-
-CREATE OR REPLACE VIEW public.vw_preco_brent
+CREATE OR REPLACE VIEW ipea.vw_preco_brent
  AS
- SELECT data,
-    EXTRACT(year FROM data) AS ano,
-    preco AS preco_atual,
-    lag(preco, 1) OVER (ORDER BY data) AS preco_anterior,
-    round(((preco - lag(preco, 1) OVER (ORDER BY data)) / lag(preco, 1) OVER (ORDER BY data) * 100::double precision)::numeric, 2) AS variacao,
-    previsto
-   FROM ( SELECT preco_brent.data,
-            preco_brent.preco,
-            0 AS previsto
-           FROM ipea.preco_brent
-        UNION ALL
-         SELECT preco_previsto_brent.data,
-            preco_previsto_brent.preco_previsto,
-            1 AS previsto
-           FROM ipea.preco_previsto_brent) tb
-  ORDER BY data;
+ SELECT tb2.data,
+    tb2.ano,
+    tb2.preco_atual_brent,
+    tb2.preco_anterior_brent,
+    tb2.variacao_preco_brent,
+    tb2.previsto,
+    COALESCE(COALESCE(COALESCE(preco_dolar.preco, lag(preco_dolar.preco, 1) OVER (ORDER BY tb2.data)), lag(preco_dolar.preco, 3) OVER (ORDER BY tb2.data)), lag(preco_dolar.preco, 1) OVER (ORDER BY tb2.data)) AS preco_dolar
+   FROM ( SELECT tb.data,
+            EXTRACT(year FROM tb.data) AS ano,
+            tb.preco AS preco_atual_brent,
+            lag(tb.preco, 1) OVER (ORDER BY tb.data) AS preco_anterior_brent,
+            round(((tb.preco - lag(tb.preco, 1) OVER (ORDER BY tb.data)) / lag(tb.preco, 1) OVER (ORDER BY tb.data) * 100::double precision)::numeric, 2) AS variacao_preco_brent,
+            tb.previsto
+           FROM ( SELECT preco_brent.data,
+                    preco_brent.preco,
+                    0 AS previsto
+                   FROM ipea.preco_brent
+                UNION ALL
+                 SELECT preco_previsto_brent.data,
+                    preco_previsto_brent.preco_previsto,
+                    1 AS previsto
+                   FROM ipea.preco_previsto_brent) tb
+          ORDER BY tb.data) tb2
+     LEFT JOIN ipea.preco_dolar ON tb2.data = preco_dolar.data;
 
-ALTER TABLE public.vw_preco_brent
+ALTER TABLE ipea.vw_preco_brent
     OWNER TO postgres;
-
